a0fcc30fc311470ac8a2bbae520fb045a64ec42a
momojohobo <momojo@rocketship.com>
2019-04-12 22:03:58 +0200

    removed .build_assets

--- .built_assets/pman_renderer.py
+++
@@ -1,7 +0,0 @@
-
-import functools
-def get_renderer():
-    modname = 'pman.nullrenderer'
-    attrs = ('NullRenderer',)
-    module =  __import__(modname, fromlist=['__name__'], level=0)
-    return functools.reduce(getattr, attrs, module)
