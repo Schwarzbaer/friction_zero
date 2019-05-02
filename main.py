b4260cd1b306334153e0c6785afff9e5c0353516
momojohobo <momojo@rocketship.com>
2019-05-02 22:50:25 +0200

    load lab from subfolder

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ panda3d.core.load_prc_file(
 
 
 class GameApp(ShowBase):
-    def __init__(self, map="assets/maps/lab.bam"):
+    def __init__(self, map="assets/maps/lab/lab.bam"):
         ShowBase.__init__(self)
         pman.shim.init(self)
         self.accept('escape', sys.exit)
