bce1e4af5a72ad57cf3b953334d13e6747eac3e0
momojohobo <momojo@rocketship.com>
2019-05-05 13:26:54 +0200

    rebuilt bams

--- assets/pman_renderer.py
+++ assets/pman_renderer.py
@@ -1,7 +1,7 @@
 
 import functools
 def get_renderer():
-    modname = 'pman.nullrenderer'
-    attrs = ('NullRenderer',)
+    modname = 'pman.basicrenderer'
+    attrs = ('BasicRenderer',)
     module =  __import__(modname, fromlist=['__name__'], level=0)
     return functools.reduce(getattr, attrs, module)
